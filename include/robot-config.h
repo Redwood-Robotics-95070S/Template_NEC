5e41071bcd05519fc2e4cf19491cec8219cceefa
VY <122563505+Astlantix@users.noreply.github.com>
2023-03-20 13:43:45 -0700

    Update robot-config.h

--- include/robot-config.h
+++ include/robot-config.h
@@ -4,11 +4,12 @@ extern brain Brain;
 
 // VEXcode devices
 extern controller Controller1;
-extern gps GPS11;
 extern motor fl;
 extern motor fr;
 extern motor bl;
 extern motor br;
+extern gps GPST;
+extern inertial Inertial;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
